62b16c731ba0f1f7c3b7482fa39dcf15346b18a0
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2025-10-26 10:13:54 -0400

    rendered html

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require("express");
 const app = express();
 const port = 4000;
 const cors = require("cors");
+const path = require("path");
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(cors());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cors());
 
-app.get("/", cors(), async (req, res) => {
-    res.send("Good to go!")
-});
+// app.get("/", cors(), async (req, res) => {
+//     res.send("Good to go!")
+// });
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./views/index.html"))
+);
 
 app.listen(port, () => {
-    console.log(`server is active at http://localhost:${port}`)
+  console.log(`server is active at http://localhost:${port}`);
 });
